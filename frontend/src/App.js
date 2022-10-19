0099e6d23da263340d3ce0053daef9e4b81c7c75
sulaiman <sulaimanshinwari830@gmail.com>
2022-10-19 22:09:09 +0500

    components remaining work done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ExercisesList from './components/ExerciseList/ExerciseContainer'
-import Navbar from './components/navbar.component';
-import EditExercise from './components/edit-exercise.compoent';
-import CreateExercise from './components/create-exercise.component';
-import CreateUser from './components/create-user.component';
+import ExercisesList from "./components/exercise-list.component";
+import Navbar from "./components/navbar.component";
+import EditExercise from "./components/edit-exercise.compoent";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (

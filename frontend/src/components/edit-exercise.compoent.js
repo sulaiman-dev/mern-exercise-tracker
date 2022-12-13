5a6a258d47401c2ad80feb4e783684a6ffe7b03c
sulaiman <sulaimanshinwari830@gmail.com>
2022-12-13 20:20:29 +0500

    moved utils directory out of components dir to src

--- frontend/src/components/edit-exercise.compoent.js
+++ frontend/src/components/edit-exercise.compoent.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import axios from "axios";
-import { withRouter } from "./utils/withRouter";
+import { withRouter } from "../utils/withRouter";
 
 class EditExercise extends Component {
   constructor(props) {
